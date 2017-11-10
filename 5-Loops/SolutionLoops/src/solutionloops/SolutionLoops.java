ae605daeaef2afc7dde2620dd2898112322915df
Pondi Brian <pondibrian@gmail.com>
2017-11-10 05:36:46 +0300

    Loops understood

---
+++ 5-Loops/SolutionLoops/src/solutionloops/SolutionLoops.java
@@ -0,0 +1,22 @@
+
+package solutionloops;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class SolutionLoops {
+    
+
+       public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+            int n = in.nextInt();       
+
+        for (int i =1; i <=10; i++){
+            int mult = n*i;
+            System.out.println(n + " x " + i + " = " + mult); 
+        }
+    }
+}
