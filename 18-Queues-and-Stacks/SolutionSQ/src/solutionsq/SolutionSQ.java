52ea58f975f37b228697ff4e5f03a93f79e48be1
Pondi Brian <pondibrian@gmail.com>
2017-12-14 08:27:52 +0300

    Updated

--- 18-Queues-and-Stacks/SolutionSQ/src/solutionsq/SolutionSQ.java
+++ 18-Queues-and-Stacks/SolutionSQ/src/solutionsq/SolutionSQ.java
@@ -1,11 +1,28 @@
 
 package solutionsq;
-import java.io.*;
 import java.util.*;
 
 
 public class SolutionSQ {
       // Write your code here.
+    Stack<Character> stack = new Stack<>();
+    Queue<Character> queue = new LinkedList<>();
+
+    void pushCharacter(Character ch) {
+       stack.push(ch);
+    }
+
+    void enqueueCharacter(char ch) {
+        queue.add(ch);
+    }
+
+    char popCharacter(){
+       return stack.pop();
+    }
+
+    char dequeueCharacter() {
+        return queue.remove();
+    } 
    
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
