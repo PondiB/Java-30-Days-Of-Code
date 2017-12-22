0bc67d0874674943dc20d2ca9a2b21442c0eed1c
Pondi Brian <pondibrian@gmail.com>
2017-12-22 17:11:14 +0300

    BST initialised

---
+++ 22-Binary-Search-Trees/SolutionBinary/src/SolutionBinary.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Node{
+    Node left,right;
+    int data;
+    Node(int data){
+        this.data=data;
+        left=right=null;
+    }
+}
+class SolutionBinary {
+	public static int getHeight(Node root){
+      //Write your code here
+    } 
+}
