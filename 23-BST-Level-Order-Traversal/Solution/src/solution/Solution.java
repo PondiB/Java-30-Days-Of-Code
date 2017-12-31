eeff3215d2c569a8cb4012fe575ee7dd6478c3b9
Pondi Brian <pondibrian@gmail.com>
2017-12-31 22:18:01 +0300

    Updated Revisit

--- 23-BST-Level-Order-Traversal/Solution/src/solution/Solution.java
+++ 23-BST-Level-Order-Traversal/Solution/src/solution/Solution.java
@@ -14,8 +14,21 @@ class Solution{
     
 static void levelOrder(Node root){
       //Write your code here
-      
+      Queue<Node> queue = new LinkedList<>();
+    if (root != null) {
+        queue.add(root);
     }
+    while ( ! queue.isEmpty()) {
+        Node n = queue.remove();
+        System.out.print(n.data + " ");
+        if (n.left != null) {
+            queue.add(n.left);
+        }
+        if (n.right != null) {
+            queue.add(n.right);
+        }
+    }   
+  }
 public static Node insert(Node root,int data){
         if(root==null){
             return new Node(data);
