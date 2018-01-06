7855fd0c7b40db97158c73d9661636970ceb4129
Pondi Brian <pondibrian@gmail.com>
2018-01-06 19:37:14 +0300

    Nested Logic Initialized

---
+++ 26-Nested-Logic/SolutionNested Logic/src/solutionnested/logic/SolutionNestedLogic.java
@@ -0,0 +1,12 @@
+
+package solutionnested.logic;
+import java.io.*;
+import java.util.*;
+public class SolutionNestedLogic {
+
+ 
+    public static void main(String[] args) {
+         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+    
+}
