cf3cb60a2c3ef2b84f7f86154c78e836adb140e9
Pondi Brian <pondibrian@gmail.com>
2018-01-07 17:59:25 +0300

    Testing Initialized

---
+++ 27-Testing/SolutionTesting/src/solutiontesting/SolutionTesting.java
@@ -0,0 +1,11 @@
+package solutiontesting;
+
+import java.io.*;
+import java.util.*;
+
+public class SolutionTesting {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. */
+    }
+}
