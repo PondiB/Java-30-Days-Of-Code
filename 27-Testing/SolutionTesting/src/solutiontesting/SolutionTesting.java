1ae91bb28758b5aaa2ed1d3b9873e848a91f012e
Pondi Brian <pondibrian@gmail.com>
2018-01-08 23:45:36 +0300

    Testing Finalized

--- 27-Testing/SolutionTesting/src/solutiontesting/SolutionTesting.java
+++ 27-Testing/SolutionTesting/src/solutiontesting/SolutionTesting.java
@@ -1,11 +1,17 @@
 package solutiontesting;
 
-import java.io.*;
-import java.util.*;
-
-public class SolutionTesting {
-
-    public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. */
+class SolutionTesting {
+    public static void main(String srgs[]) {
+        System.out.println(5);
+        System.out.println("4 3");
+        System.out.println("0 -3 4 2");
+        System.out.println("5 2");
+        System.out.println("0 -3 4 2 2");
+        System.out.println("3 3");
+        System.out.println("0 -3 4");
+        System.out.println("7 2");
+        System.out.println("0 -3 1 1 1 1 1");
+        System.out.println("6 3");
+        System.out.println("0 -3 4 2 1 1");
     }
 }
