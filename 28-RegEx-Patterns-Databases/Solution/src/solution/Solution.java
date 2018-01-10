442224ebcaf5e0fe18966400d84b131a2a8c5c73
Pondi Brian <pondibrian@gmail.com>
2018-01-10 13:57:24 +0300

    Finailzed

--- 28-RegEx-Patterns-Databases/Solution/src/solution/Solution.java
+++ 28-RegEx-Patterns-Databases/Solution/src/solution/Solution.java
@@ -1,11 +1,29 @@
 package solution;
 
-import java.io.*;
 import java.util.*;
 
 public class Solution {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+        
+        ArrayList<String> names = new ArrayList<String>();
+        
+        int n = in.nextInt();
+        for (int i = 0; i < n; i++){
+            String name = in.next();
+            String email = in.next();
+            
+            if(email.contains("@gmail")){
+                names.add(name);
+            }            
+        }
+        
+        Collections.sort(names);
+        
+        for (String firstName : names){
+            System.out.println(firstName);
+        }
     }
 }
