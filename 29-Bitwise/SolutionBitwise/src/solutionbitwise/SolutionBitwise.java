a4a73f56e053a097a22208bc86972af149fdae5c
Pondi Brian <pondibrian@gmail.com>
2018-01-16 12:50:31 +0300

    Done

--- 29-Bitwise/SolutionBitwise/src/solutionbitwise/SolutionBitwise.java
+++ 29-Bitwise/SolutionBitwise/src/solutionbitwise/SolutionBitwise.java
@@ -1,12 +1,31 @@
 
 package solutionbitwise;
 
+import java.util.*;
 
 public class SolutionBitwise {
-
  
     public static void main(String[] args) {
         // TODO code application logic here
+        
+                Scanner in = new Scanner(System.in);
+        int T = in.nextInt();
+
+        for (int i = 0; i < T; i++) {
+            int N = in.nextInt();
+            int K = in.nextInt();
+
+            int max = 0;
+
+            for (int j = 1; j < N; j++) {
+                for (int k = j + 1; k <= N; k++) {
+                    int h = j & k;
+                    if (h < K && max < h) max = h;
+                }
+            }
+
+            System.out.println(max);
+        }
     }
     
 }
